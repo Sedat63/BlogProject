1e45e79e8c8149309f849ee14b2724c2ea521e99
Sedat63 <sedatozturk63@yandex.com>
2022-09-17 11:39:42 +0300

    17.09

--- Entities/Concrete/Article.cs
+++ Entities/Concrete/Article.cs
@@ -7,7 +7,6 @@ namespace Entities.Concrete
 {
     public class Article : IEntity
     {
-
         public int Id { get; set; }
         public string Title { get; set; }  
         public string Contents { get; set; }
